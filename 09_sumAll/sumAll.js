9352870c08810e0fe0e39f1cba444f81194c5662
Daniel Lee <57880066+jughkoop@users.noreply.github.com>
2025-08-06 12:57:50 -0600

    Complete 9th part of TOP JavaScript assignment

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if((typeof a !== "number" || a < 0) || (typeof b !== "number" || b < 0)) return "ERROR";
+    else if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
 
+    if(a > b) {
+        const temp = a;
+        a = b;
+        b = temp;
+    }
+    let sum = 0;
+    for(let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
